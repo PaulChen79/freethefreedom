7eb6b6374e2af849131897f7077dafde7f228fee
Pin Hsu Chen <paul791111@gmail.com>
2022-06-08 12:32:20 +0800

    setup commit

---
+++ app.js
@@ -0,0 +1,35 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const express = require('express')
+const exphbs = require('express-handlebars')
+const { handlebarsHelpers } = require('./helpers/handlebars-helpers')
+const session = require('express-session')
+const flash = (require('connect-flash'))
+const methodOverride = require('method-override')
+// const path = require('path')
+const routes = require('./routes/index')
+const app = express()
+const port = process.env.PORT || 3000
+require('./config/mongoose')
+
+app.engine('hbs', exphbs.engine({ defalutLayouts: 'main', extname: '.hbs', helpers: handlebarsHelpers }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use(methodOverride('_method'))
+// app.use('/upload', express.static(path.join(__dirname, 'upload')))
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.user = req.user
+  next()
+})
+
+app.use(routes)
+
+app.listen(port, () => {
+  console.info(`Example app listening on port ${port}!`)
+})
